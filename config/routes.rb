1f4dcb82156f49184001e51fa385f96dbc3a0391
Cassidy Scheffer <hello@cassidy.codes>
2025-04-28 07:13:30 -0400

    wip -- creating dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
+  namespace :dashboard do
+    get "posts/new"
+    get "posts/create"
+    get "posts/edit"
+    get "posts/update"
+    get "posts/destroy"
+  end
+  get "dashboard/show"
   resources :posts
   resource :session
   resources :passwords, param: :token
+  get "dashboard" => "dashboard#show", as: :dashboard
   get "up" => "rails/health#show", as: :rails_health_check
-  root "rails/health#show"
+  root "home#show"
 end
