89a9331ade4d42e53f2f4c1e60a30ea2015364b7
Cassia Scheffer <cassia@fastmail.com>
2025-05-19 17:22:51 -0400

    wip

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,25 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: "User", foreign_key: "author_id"
+  before_save :set_slug
+  before_create :set_published_at
+
+  def to_key
+    [ self.slug ]
+  end
+
+  private
+    def set_slug
+      if self.slug.blank?
+        return self.slug = "#{self.title.parameterize}-#{SecureRandom.hex(4)}"
+      end
+      if Post.where(slug: self.slug).where.not(id: self.id).exists?
+        self.slug = "#{self.slug}-#{SecureRandom.hex(4)}"
+      end
+    end
+
+    def set_published_at
+      if self.published && self.published_at.blank?
+        self.published_at = DateTime.now
+      end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 Rails.application.routes.draw do
-  namespace :dashboard do
-    get "posts/new"
-    get "posts/create"
-    get "posts/edit"
-    get "posts/update"
-    get "posts/destroy"
-  end
-  get "dashboard/show"
-  resources :posts
   resource :session
   resources :passwords, param: :token
   get "dashboard" => "dashboard#show", as: :dashboard
+  namespace :dashboard do
+    resources :posts, except: %i[ index show ]
+  end
+  constraints(subdomain: /.+/) do
+    get "/", to: "posts#index", as: :posts
+    get "/:slug", to: "posts#show", as: :post
+  end
   get "up" => "rails/health#show", as: :rails_health_check
   root "home#show"
 end
