a33e2e22e6ae7c9156c02c55f5b462b1653b7385
Cassia Scheffer <cassia@fastmail.com>
2025-05-19 17:22:53 -0400

    sign up

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      start_new_session_for(@user)
+      redirect_to after_authentication_url, notice: "Welcome to the site!"
+    else
+      render :new
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email_address, :password, :password_confirmation, :subdomain, :name)
+    end
+end
