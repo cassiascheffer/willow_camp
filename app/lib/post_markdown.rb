ac53c7f046bf8a32f0174eb359eeda856afb49de
Cassia Scheffer <cassia@enumerator.dev>
2025-06-12 08:33:10 -0400

    class mermaid

--- app/lib/post_markdown.rb
+++ app/lib/post_markdown.rb
@@ -31,7 +31,7 @@ class PostMarkdown
   def process_mermaid_blocks(html)
     # Add mermaid controller to pre elements with mermaid lang
     html.gsub('<pre lang="mermaid"') do |match|
-      '<pre lang="mermaid" data-controller="mermaid"'
+      '<pre lang="mermaid" data-controller="mermaid" class="mermaid"'
     end
   end
 end
