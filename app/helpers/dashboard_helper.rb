95c8eb9c2f767748b6ebf4bbdf97d509c8ad11a0
Cassia Scheffer <cassia@enumerator.dev>
2025-06-27 17:02:24 -0400

    clean up themes

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,3 +1,2 @@
 module DashboardHelper
-  ALL_THEMES = %w[light dark abyss acid aqua autumn black bumblebee business caramellatte cmyk coffee corporate cupcake cyberpunk dim dracula emerald fantasy forest garden halloween lemonade lofi luxury night nord pastel retro silk sunset synthwave valentine vineframe winter].freeze
 end
