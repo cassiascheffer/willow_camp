7fbb6061bf7c3e7772adde42753f713e69661a08
Cassia Scheffer <cassia@enumerator.dev>
2025-07-21 07:27:37 -0400

    move settins

--- test/controllers/dashboard/settings/about_pages_controller_test.rb
+++ test/controllers/dashboard/settings/about_pages_controller_test.rb
@@ -19,10 +19,10 @@ class Dashboard::Settings::AboutPagesControllerTest < ActionDispatch::Integratio
         }
       }
     end
-    
+
     assert_redirected_to dashboard_settings_path
     assert_equal "Created!", flash[:notice]
-    
+
     # Find the page by title since FriendlyId might change the slug
     page = Page.find_by(title: "About Me", author: @user)
     assert_not_nil page, "Page should exist with title 'About Me'"
@@ -42,7 +42,7 @@ class Dashboard::Settings::AboutPagesControllerTest < ActionDispatch::Integratio
         }
       }, as: :turbo_stream
     end
-    
+
     assert_response :success
     assert_match "You now have an about page. Nice!", response.body
   end
@@ -57,11 +57,10 @@ class Dashboard::Settings::AboutPagesControllerTest < ActionDispatch::Integratio
         }
       }, as: :turbo_stream
     end
-    
+
     assert_response :success
     assert_match "about-page-form", response.body
   end
-
 
   test "should update about page with valid params" do
     patch dashboard_settings_about_page_url(slug: @page.slug), params: {
@@ -70,10 +69,10 @@ class Dashboard::Settings::AboutPagesControllerTest < ActionDispatch::Integratio
         body_markdown: "Updated content"
       }
     }
-    
+
     assert_redirected_to dashboard_settings_path
     assert_equal "Updated!", flash[:notice]
-    
+
     @page.reload
     assert_equal "Updated About", @page.title
     assert_equal "Updated content", @page.body_markdown
@@ -86,7 +85,7 @@ class Dashboard::Settings::AboutPagesControllerTest < ActionDispatch::Integratio
         body_markdown: "Updated content"
       }
     }, as: :turbo_stream
-    
+
     assert_response :success
     assert_match "Updated!", response.body
   end
@@ -98,19 +97,18 @@ class Dashboard::Settings::AboutPagesControllerTest < ActionDispatch::Integratio
         slug: ""
       }
     }, as: :turbo_stream
-    
+
     assert_response :success
     assert_match "about-page-form", response.body
     @page.reload
     assert_not_equal "", @page.title
   end
 
-
   test "should destroy about page" do
     assert_difference("Page.count", -1) do
       delete dashboard_settings_about_page_url(slug: @page.slug)
     end
-    
+
     assert_redirected_to dashboard_settings_path
     assert_equal "Page was successfully deleted.", flash[:notice]
   end
@@ -119,10 +117,10 @@ class Dashboard::Settings::AboutPagesControllerTest < ActionDispatch::Integratio
     assert_difference("Page.count", 0) do
       delete dashboard_settings_about_page_url(slug: @page.slug), as: :turbo_stream
     end
-    
+
     assert_response :success
     assert_match "Page was successfully deleted.", response.body
-    
+
     # Check that a new about page was created
     new_about = @user.pages.find_by(slug: "about")
     assert_not_nil new_about
@@ -132,28 +130,28 @@ class Dashboard::Settings::AboutPagesControllerTest < ActionDispatch::Integratio
   test "should not find page for different user" do
     # Ensure page belongs to first user
     assert_equal @user.id, @page.author_id
-    
+
     # Create a second user and sign them in
     other_user = users(:two)
     sign_out @user
     sign_in other_user
-    
+
     # The controller should not find the page since it belongs to a different user
     # Rails handles RecordNotFound and returns a 404
     patch dashboard_settings_about_page_url(slug: @page.slug), params: {
-      page: { title: "Hacked!" }
+      page: {title: "Hacked!"}
     }
-    
+
     assert_response :not_found
   end
 
   test "should require authentication" do
     sign_out @user
-    
+
     post dashboard_settings_about_pages_url, params: {
-      page: { title: "Test" }
+      page: {title: "Test"}
     }
-    
+
     assert_redirected_to new_user_session_path
   end
 end
