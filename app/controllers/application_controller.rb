c0a571a0dbdf7eaa0ac7a110ec3d60c4519b7e47
Cassia Scheffer <cassia@enumerator.dev>
2025-07-30 07:54:29 -0400

    quite rack attack, add data to honeybadger

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,13 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
+  before_action do
+    Honeybadger.context({
+      user_subdomain: current_user&.subdomain,
+      current_host: request.host
+    })
+  end
+
   protected
 
   def redirect_if_authenticated
