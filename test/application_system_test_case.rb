39f0ff12053b5e9f92b8ec069af29296d432d9c7
Cassia Scheffer <cassia@enumerator.dev>
2025-08-11 19:57:15 -0400

    disable selenium chrome password detection

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -9,6 +9,19 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
       url: "http://#{ENV["SELENIUM_HOST"]}:4444"
     }
   else
-    driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+    driven_by :selenium, using: :chrome, screen_size: [1400, 1400] do |driver_option|
+      driver_option.add_argument('--headless=new')
+      driver_option.add_argument('--no-sandbox')
+      driver_option.add_argument('--disable-dev-shm-usage')
+
+      # Disable password protection features
+      driver_option.add_argument('--disable-features=PasswordLeakDetection')
+      driver_option.add_argument('--disable-password-generation')
+      driver_option.add_argument('--disable-password-manager-reauthentication')
+
+      # Set preferences to disable password features
+      driver_option.add_preference('profile.password_manager_enabled', false)
+      driver_option.add_preference('profile.password_manager_leak_detection', false)
+    end
   end
 end
